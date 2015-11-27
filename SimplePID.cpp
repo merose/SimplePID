058d0e8c2504a3a76e40db4b2e6821f3bc4e295b
Mark Rose <markrose@acm.org>
2015-11-27 02:59:06 -0800

    Initial checkin of PID code.

---
+++ SimplePID.cpp
@@ -0,0 +1,55 @@
+#include <float.h>
+#include "SimplePID.h"
+
+SimplePID::SimplePID(float Kp, float Ki, float Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
+
+  minOutput = -FLT_MAX;
+  maxOutput = FLT_MAX;
+  
+  sumError = 0.0;
+  lastActual = 0.0;
+}
+  
+void SimplePID::setOutputRange(float minOutput, float maxOutput) {
+  this->minOutput = minOutput;
+  this->maxOutput = maxOutput;
+}
+
+void SimplePID::setSetPoint(float setPoint) {
+  this->setPoint = setPoint;
+  sumError = 0.0;
+}
+
+float SimplePID::getCumulativeError() {
+  return sumError;
+}
+
+void SimplePID::clearCumulativeError() {
+  sumError = 0.0;
+}
+
+float SimplePID::getControlValue(float actual, float dt) {
+  float error = setPoint - actual;
+
+  float newSum = sumError + error*dt;
+
+  // The derivative is calculated by assuming the two setpoints are
+  // equal. This works better when changing the setpoint because the
+  // derivative error does not suddenly increase.
+  float dErrorDt = (lastActual - actual) / dt;
+  lastActual = actual;
+
+  float output = Kp*error + Ki*sumError + Kd*dErrorDt;
+
+  if (output >= maxOutput) {
+    return maxOutput;
+  } else if (output <= minOutput) {
+    return minOutput;
+  } else {
+    sumError = newSum;
+    return output;
+  }
+}
---
+++ SimplePID.h
@@ -0,0 +1,34 @@
+// SimplePID.h - Definitions for the simple PID library.
+
+#ifndef SIMPLE_PID_H
+#define SIMPLE_PID_H
+
+class SimplePID {
+  public:
+
+    SimplePID(float Kp, float Ki, float Kd);
+  
+    void setOutputRange(float minOutput, float maxOutput);
+
+    void setSetPoint(float setPoint);
+
+    float getCumulativeError();
+
+    void clearCumulativeError();
+
+    float getControlValue(float actual, float dt);
+
+  private:
+  
+    float Kp;
+    float Ki;
+    float Kd;
+    float setPoint;
+    float minOutput;
+    float maxOutput;
+    float lastActual;
+    float sumError;
+
+};
+
+#endif SIMPLE_PID_H
